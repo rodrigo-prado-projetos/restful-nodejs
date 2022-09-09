3149aa183ff0a5d7961fb4d3d03e4b321d3c946f
Rodrigo Prado <digosilva.prado@gmail.com>
2022-09-09 14:02:32 -0300

    Primeira versão

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const express = require ('express')
 const cors = require('cors');
 const path = require ('path')
 const app = express ()
-const apiRouter = require('./api/routes/api_routes')
+const apiRouter = require('./api/routes/apiRouter')
 
 
 app.use(cors())
