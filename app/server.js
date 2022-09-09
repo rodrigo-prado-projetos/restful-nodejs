bd4cc6492e54bf567588404ab348cc21d9ed6a18
Rodrigo Prado <digosilva.prado@gmail.com>
2022-09-09 13:45:08 -0300

    Primeira versão

--- app/server.js
+++ app/server.js
@@ -4,14 +4,14 @@ const express = require ('express')
 const cors = require('cors');
 const path = require ('path')
 const app = express ()
-//const apiRouter = require('./api/routes/api_routes')
+const apiRouter = require('./api/routes/api_routes')
 
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/app', express.static (path.join (__dirname, '/public')))
-//app.use ('/api', apiRouter)
+app.use ('/api', apiRouter)
 
 let port = process.env.PORT || 3000
 app.listen (port)
